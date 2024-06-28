bd2bffee6648363b79363529fdd6164ae85b1874
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-06-28 13:39:06 +0530

    added rpoxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: 5000 // Correct way to specify the port
-  // }
+  server: {
+    proxy: {
+      // Proxying API requests to avoid CORS issues during development
+      '/api': 'https://login-signup-react-t8ka.onrender.com'
+    }
+}
 })
