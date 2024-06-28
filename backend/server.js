06b0dccb4abf22979d1f2765b1026e6c187fe248
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-06-28 14:30:52 +0530

    changed routes

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ connectDB();
 // Routes
 app.use('/api/auth', authRoutes); 
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
