4da3e9848c78d4b6dc1dc8f38ee6935179da4cfc
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-06-28 14:36:30 +0530

    route handling

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,15 @@ app.use(cookieParser());
 
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "./dist")));
+console.log(path.join(__dirname, "./dist"));
 
 connectDB();
 
 // Routes
 app.use('/api/auth', authRoutes); 
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
