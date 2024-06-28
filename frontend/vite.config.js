4d514b1c53e7e1542070b71dedef733d81dbb7f5
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-06-28 14:52:30 +0530

    initial

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Proxying API requests to avoid CORS issues during development
-      '/api': 'https://login-signup-react-t8ka.onrender.com'
-    }
-}
+  // server: {
+  //   proxy: {
+  //     // Proxying API requests to avoid CORS issues during development
+  //     '/api': 'https://login-signup-react-t8ka.onrender.com'
+  //   }
+// }
 })
